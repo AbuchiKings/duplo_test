479a692065a4caf6d21dc1d2e862efef71292898
abuchikings <abuchikingsley76@gmail.com>
2023-10-27 09:21:03 +0100

    Updated errorHandler

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -4,20 +4,13 @@ import { ApiError, InternalError } from './ApiError';
 import { ConflictMsgResponse, UnprocessibleEntityResponse } from './ApiResponse';
 
 const errorHandler = (err: any, req: FastifyRequest, res: FastifyReply,) => {
-    
-    if (err instanceof ApiError) {
-        ApiError.handle(err, res);
+
+    if (err.code === 'FST_ERR_VALIDATION') {
+        return new UnprocessibleEntityResponse(err.message).send(res);
+    } else if (err.code === 'P2002') {
+        return new ConflictMsgResponse(`${err.meta?.target[0]} is already in use`).send(res)
     } else {
-        if (err.code === 'FST_ERR_VALIDATION') {
-            return new UnprocessibleEntityResponse(err.message).send(res);
-        } else if (err.code === 'P2002') {
-            return new ConflictMsgResponse(`${err.meta?.target[0]} is already in use`)
-        }
-        if (process.env.NODE_ENV === 'development') {
-            //console.log(err) // eslint-disable-line
-            return res.status(500).send(err.message);
-        }
-        ApiError.handle(new InternalError(), res);
+        ApiError.handle(err, res);
     }
 }
 
